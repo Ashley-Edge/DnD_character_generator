1da42b8a41888f246abc3d9ecd66fca05ff840f1
Ashley-Edge <ashleyedge89@gmail.com>
2021-02-28 19:29:13 +0000

    Saving for the evening

--- database/User.sql
+++ database/User.sql
@@ -1,2 +1,2 @@
     CREATE USER 'ashley'@'172.18.0.2' identified BY 'root';
-    GRANT ALL PRIVILEGES ON characer_db.* TO 'ashley'@'172.18.0.2';
+    GRANT ALL PRIVILEGES ON character_db.* TO 'ashley'@'172.18.0.2';
