54b7fdf4825f3ef735d431dc6f12e4f64e3f9783
Ashley-Edge <ashleyedge89@gmail.com>
2021-03-01 03:50:08 +0000

    Docker-compose is working

--- database/User.sql
+++ database/User.sql
@@ -1,2 +1,13 @@
-    CREATE USER 'ashley'@'172.18.0.2' identified BY 'root';
-    GRANT ALL PRIVILEGES ON character_db.* TO 'ashley'@'172.18.0.2';
+    -- CREATE USER 'ashley'@'172.18.0.2' identified BY 'root';
+    -- GRANT ALL PRIVILEGES ON character_db.* TO 'ashley'@'172.18.0.2';
+    -- CREATE USER 'ashley'@'172.19.0.2' identified BY 'root';
+    -- GRANT ALL PRIVILEGES ON *.* TO 'ashley'@'172.18.0.2';
+
+    -- CREATE USER 'ashley'@'172.19.0.5' identified BY '1234';
+    -- GRANT ALL PRIVILEGES ON character_db.* TO 'ashley'@'172.19.0.5';
+    -- CREATE USER 'ashley'@'172.19.0.5' identified BY 'root';
+    -- GRANT ALL PRIVILEGES ON *.* TO 'ashley'@'172.19.0.5';
+
+    CREATE USER 'ashley'@'%' identified BY '1234';
+    GRANT ALL PRIVILEGES ON character_db.* TO 'ashley'@'%';
+    GRANT ALL PRIVILEGES ON *.* TO 'ashley'@'%';
