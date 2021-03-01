262790296b299d7e6b19bb2c4d65b498dd1fa026
Ashley-Edge <ashleyedge89@gmail.com>
2021-03-01 02:18:54 +0000

    Working on docker compose

--- database/User.sql
+++ database/User.sql
@@ -1,2 +1,4 @@
     CREATE USER 'ashley'@'172.18.0.2' identified BY 'root';
     GRANT ALL PRIVILEGES ON character_db.* TO 'ashley'@'172.18.0.2';
+    CREATE USER 'ashley'@'172.19.0.2' identified BY 'root';
+    GRANT ALL PRIVILEGES ON character_db.* TO 'ashley'@'172.18.0.2';
