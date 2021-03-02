938c712a6853289d801972cd41f5e75d83b988d6
Ashley-Edge <ashleyedge89@gmail.com>
2021-03-02 03:45:49 +0000

    attempting jenkins again

--- scripts/apptesting.sh
+++ scripts/apptesting.sh
@@ -1,24 +1,24 @@
 # Install dependencies
-sudo update -y
-sudo upgrade -y
+sudo apt update
+sudo apt upgrade 
 sudo apt install -y python3 python3-pip
 
 # cd into service1, install requirements and test it
 cd service1
 pip3 install -r requirements.txt
-python3 -m pytest --cov app
+pytest --cov app
 
 # cd into service2 and test it
 cd ../service2
-python3 -m pytest --cov app
+python3 -m pytest --cov application
 
 # cd into service3 and test it
 cd ../service3
-python3 -m pytest --cov app
+python3 -m pytest --cov application
 
 # cd into service4 and test it
 cd ../service4
-python3 -m pytest --cov app
+pytest --cov application
 
 # Return to root
 cd ..
