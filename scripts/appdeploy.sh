be9aa6864e9dc5f572f0249f73d1c16d1bb33cb7
Ashley-Edge <ashleyedge89@gmail.com>
2021-03-12 18:41:04 +0000

    de-bugging jenkins deploy stage3

--- scripts/appdeploy.sh
+++ scripts/appdeploy.sh
@@ -1,8 +1,9 @@
 # Login dnd-managerz
-ssh 10.172.0.15 << EOF
+# ssh 10.172.0.15 << EOF
+ssh -i ~/.ssh/jenkins_agent_key swarmanager << EOF
 sudo rm -rf DnD_character_generator
 git clone https://github.com/Ashley-Edge/DnD_character_generator.git
 cd DnD_character_generator
-sudo docker login
+sudo docker login -u ashley -p PeneloPig1989
 sudo docker stack deploy --compose-file docker-compose.yaml dnd
 EOF
