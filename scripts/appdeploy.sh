9a67653735ed6ce0e705b2da9dc6c39f0fc5e644
Ashley-Edge <ashleyedge89@gmail.com>
2021-03-12 18:48:14 +0000

    de-bugging jenkins deploy stage4

--- scripts/appdeploy.sh
+++ scripts/appdeploy.sh
@@ -1,6 +1,6 @@
 # Login dnd-managerz
 # ssh 10.172.0.15 << EOF
-ssh -i ~/.ssh/jenkins_agent_key swarmanager << EOF
+ssh -i ~/etc/ssh/ssh_host_rsa_key 10.172.0.15 << EOF
 sudo rm -rf DnD_character_generator
 git clone https://github.com/Ashley-Edge/DnD_character_generator.git
 cd DnD_character_generator
